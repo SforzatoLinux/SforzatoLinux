bb96ca9e35c00778c3fefa8f76b8c135a46e1ef0
aruelu <arugacchi@gmail.com>
2024-03-16 01:02:16 +0900

    config.shからArcMenuの導入を削除

--- test-gui-04/config.sh
+++ test-gui-04/config.sh
@@ -40,13 +40,6 @@ suseInsertService NetworkManager
 baseSetRunlevel 5
 
 #======================================
-# ArcMenu(GNOME拡張機能)のインストール
-#--------------------------------------
-cd /usr/share/gnome-shell/extensions/
-gnome-extensions install arc-menu@arc-menu.com
-gnome-extensions enable arc-menu@arc-menu.com
-
-#======================================
 # Firefox(Flatpakから)のインストール
 #--------------------------------------
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
